6e2a35bb41b058d0b8a431f179d27ae932f46694
LeTueurV <leshautiens@gmail.com>
2024-11-13 21:55:07 +0100

    refonte du site

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+async function hashPassword(password) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(password);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+    const hashArray = Array.from(new Uint8Array(hashBuffer)); // Convert buffer to byte array
+    const hashHex = hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
+    return hashHex; // Retourne le hachage sous forme de chaîne hexadécimale
+}
+
+
+
 function Vérif()
 {
-    var vI={"Admin" : "MDP"};
+    var vI={"Admin" : "8c8c11fb1189d882425d9bb229024689a601ba4b13a1e614d4303e15a578bb2c "};
     var identifiant = document.getElementById('i').value; 
     var motDePasse = document.getElementById('mdp').value;
-    if (vI[identifiant] === motDePasse) {
+    var hashedPassword = await hashPassword(motDePasse);
+    if (vI[identifiant] === hashedPassword) {
         window.location.href = "autorisé.html"; 
     } else {
         alert("Identifiant ou Mot de passe incorrect");
